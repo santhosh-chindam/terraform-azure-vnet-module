23eb698c3e8aa4474bd26e77eb920c70a0e6302d
santhosh-chindam <70210624+santhosh-chindam@users.noreply.github.com>
2025-07-18 21:56:44 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 output "vnet_name" {
-  value = azurerm_virtual_network.vnet.name
+  value = azurerm_virtual_network.this.name
 }
 
 output "vnet_id" {
-  value = azurerm_virtual_network.vnet.id
+  value = azurerm_virtual_network.this.id
 }
 
 output "subnet_ids" {
-  value = [for s in azurerm_subnet.subnets : s.id]
+  value = [for s in azurerm_subnet.this : s.id]
 }
 
 output "nsg_id" {
-  value = try(azurerm_network_security_group.nsg.id, null)
+  value = try(azurerm_network_security_group.this[0].id, null)
 }
